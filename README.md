bb831a2828a3aefb6226cc18be1815ac2eac3dfd
KarmaHater <andra.lally@gmail.com>
2018-04-03 16:41:04 -0400

    auth flow

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fullstackJS
+
+https://sleepy-cliffs-75117.herokuapp.com/
+
+next steps add fb provide stragey
+
+
---
+++ server/services/passport.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecert,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(oldUser => {
+        if (!oldUser) {
+          new User({ googleId: profile.id }).save().then(user => {
+            done(null, user);
+            return;
+          });
+        }
+        done(null, oldUser);
+      });
+    }
+  )
+);
